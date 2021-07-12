7746fb24d964f659b8a52416da94cddc2de0fb8f
lauren0914 <cofks05@likelion.org>
2021-07-12 18:14:28 +0900

    7/12 project2 commit 3

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = 'accountapp' # 라우팅 편리하게 하기 위해서 작성해둔 것
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world')
+    path('hello_world/', hello_world, name='hello_world'),
+    path('create/', AccountCreateView.as_view(), name='create')
 ]
