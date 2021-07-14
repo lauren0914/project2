1a28ccf7eb4dd14c08b78e7eaca57df8bed6cfbb
lauren0914 <cofks05@likelion.org>
2021-07-14 12:38:33 +0900

    7/14 project2 commit 3

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from accountapp.views import hello_world, AccountCreateView
@@ -5,5 +6,8 @@ from accountapp.views import hello_world, AccountCreateView
 app_name = 'accountapp' # 라우팅 편리하게 하기 위해서 작성해둔 것
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'),
+         name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('create/', AccountCreateView.as_view(), name='create')
 ]
