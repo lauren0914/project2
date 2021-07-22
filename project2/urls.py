5820d58d3d237f9c2e50dafc6b0d9ff6f7655be8
lauren0914 <cofks05@likelion.org>
2021-07-22 18:15:23 +0900

    7/22 project2 commit 4

--- project2/urls.py
+++ project2/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('accountapp.urls')),
-    path('profile/', include('profileapp.urls'))
+    path('profiles/', include('profileapp.urls'))
 
 ]
