eb48178c429eef27aefb5c5b1aa99d33a6d93ddd
lauren0914 <cofks05@likelion.org>
2021-07-29 17:34:51 +0900

    7/29 project2 commit 3

--- project2/urls.py
+++ project2/urls.py
@@ -22,6 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('accountapp.urls')),
-    path('profiles/', include('profileapp.urls'))
-
+    path('profiles/', include('profileapp.urls')),
+    path('articles/', include('articleapp.ulrs')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
