a39fa35340a714cfffa33ddc2ec03a5ae582b9ec
lauren0914 <cofks05@likelion.org>
2021-08-18 12:25:58 +0900

    8/18 project2 commit 2

--- projectapp/views.py
+++ projectapp/views.py
@@ -2,9 +2,9 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, ListView
 
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
@@ -21,3 +21,12 @@ class ProjectDetailView(DetailView):
     model = Project
     context_object_name = 'target_project'
     template_name = 'projectapp/detail.html'
+
+    def get_success_url(self):
+        return reverse('projectapp:detail', kwargs={'pk' : self.object.pk})
+
+class ProjectListView(ListView):
+    model = Project
+    context_object_name = 'project_list'
+    template_name = 'projectapp/list.html'
+    paginate_by = 20
