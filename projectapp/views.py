378596787aaf9096b59c420fa23ec9e5d0fda1c6
lauren0914 <cofks05@likelion.org>
2021-08-19 17:02:28 +0900

    8/19 project2 commit 1

--- projectapp/views.py
+++ projectapp/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic.list import MultipleObjectMixin
 
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
@@ -17,13 +18,21 @@ class ProjectCreateView(CreateView):
     success_url = reverse_lazy('articleapp:list')
     template_name = 'projectapp/create.html'
 
-class ProjectDetailView(DetailView):
+class ProjectDetailView(DetailView, MultipleObjectMixin):
     model = Project
     context_object_name = 'target_project'
     template_name = 'projectapp/detail.html'
 
-    def get_success_url(self):
-        return reverse('projectapp:detail', kwargs={'pk' : self.object.pk})
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        # 조건에 맞는 애들만 필터링하겠다.
+        article_list = Article.objects.filter()
+        return super().get_context_data(**kwargs)
+
+
+    # def get_success_url(self):
+    #     return reverse('projectapp:detail', kwargs={'pk' : self.object.pk})
 
 class ProjectListView(ListView):
     model = Project
