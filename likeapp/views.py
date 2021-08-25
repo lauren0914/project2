3193330f9bc1f3aef1f843a6ac809f226674be3c
lauren0914 <cofks05@likelion.org>
2021-08-25 12:16:06 +0900

    8/25 project2 commit 3

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,14 +1,17 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import RedirectView
 
 from articleapp.models import Article
 from likeapp.models import LikeRecord
 
-
+@method_decorator(login_required, 'get')
 class LikeArticleView(RedirectView):
     def get(self, request, *args, **kwargs):
         user = request.user
@@ -18,11 +21,13 @@ class LikeArticleView(RedirectView):
                                                article=article)
 
         if likeRecord.exists():
+            messages.add_message(request, messages.ERROR, '좋아요는 한 번만 가능합니다.')
             return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk':kwargs['article_pk']}))
         else:
             LikeRecord(user=user, article=article).save()
             article.like += 1
             article.save()
+            messages.add_message(request, messages.SUCCESS, '좋아요가 반영되었습니다.')
         return super().get(request, *args, **kwargs)
 
     def get_redirect_url(self, *args, **kwargs):
